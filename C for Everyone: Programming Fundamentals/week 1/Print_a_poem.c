24185b2262e84d931320ef61907d919eda5a47be
TzabarDolev <45394343+TzabarDolev@users.noreply.github.com>
2020-11-02 17:08:46 +0200

    Add files via upload

---
+++ C for Everyone: Programming Fundamentals/week 1/Print_a_poem.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void)
+{   
+    printf("This is a poem\n");
+    printf("This is a great poem\n");
+    printf("This is an amazing poem\n");
+    printf("Oh what a poem\n");
+    return 0;
+}    
